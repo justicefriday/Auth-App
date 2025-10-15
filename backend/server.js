4f841516c4026216c4998a0f2679edb6fdd2e724
justicefriday <126066170+justicefriday@users.noreply.github.com>
2025-10-15 13:37:26 +0100

    fixing of the servers

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,10 @@ app.use('/api/users', userRoutes);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist/assets')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend/dist/assets', 'index.html'));
   });
 } else {
   app.get('/', (req, res) => res.send('Server is running...'));
